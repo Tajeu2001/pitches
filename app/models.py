45264096ecec2b3f0c3e03b4c22ce5b4db5dce38
Tajeu2001 <tajeusanta@gmail.com>
2021-06-24 02:03:39 +0300

    Create a pitch  model

--- app/models.py
+++ app/models.py
@@ -57,7 +57,7 @@ class User(UserMixin,db.Model):
 
 
 
-#category model
+
 class PitchCategory(db.Model):
 
     __tablename__ = 'categories'
@@ -77,39 +77,39 @@ class PitchCategory(db.Model):
         categories = PitchCategory.query.all()
         return categories
 
-# #pitches class
-# class Pitch(db.Model):
-#     """
-#     List of pitches in each category 
-#     """
 
-#     __tablename__ = 'pitches'
+class Pitch(db.Model):
+    """
+    List of pitches in each category 
+    """
 
-#     id = db.Column(db.Integer,primary_key = True)
-#     content = db.Column(db.String())
-#     category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
-#     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-#     comment = db.relationship("Comments", backref="pitches", lazy = "dynamic")
-#     vote = db.relationship("Votes", backref="pitches", lazy = "dynamic")
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    content = db.Column(db.String())
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship("Comments", backref="pitches", lazy = "dynamic")
+    vote = db.relationship("Votes", backref="pitches", lazy = "dynamic")
 
 
 
-#     def save_pitch(self):
-#         """
-#         Save the pitches 
-#         """
-#         db.session.add(self)
-#         db.session.commit()
+    def save_pitch(self):
+        """
+        Save the pitches 
+        """
+        db.session.add(self)
+        db.session.commit()
 
-#     @classmethod
-#     def clear_pitches(cls):
-#         Pitch.all_pitches.clear()
+    @classmethod
+    def clear_pitches(cls):
+        Pitch.all_pitches.clear()
 
-#     # display pitches
+    # display pitches
 
-#     def get_pitches(id):
-#         pitches = Pitch.query.filter_by(category_id=id).all()
-#         return pitches
+    def get_pitches(id):
+        pitches = Pitch.query.filter_by(category_id=id).all()
+        return pitches
 
 
 
