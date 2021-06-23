2312eb375b768eb7573c1eaff307fe810faf1ae8
Tajeu2001 <tajeusanta@gmail.com>
2021-06-24 02:02:30 +0300

    Create a pitch category model

--- app/models.py
+++ app/models.py
@@ -50,32 +50,32 @@ class User(UserMixin,db.Model):
         return check_password_hash(self.pass_secure,password)
 
 
-#     def __repr__(self):
-#         return f'User {self.username}'
+    def __repr__(self):
+        return f'User {self.username}'
 
 
 
 
 
-# #category model
-# class PitchCategory(db.Model):
+#category model
+class PitchCategory(db.Model):
 
-#     __tablename__ = 'categories'
+    __tablename__ = 'categories'
 
-#     # table columns
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255))
-#     description = db.Column(db.String(255))
+    # table columns
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
 
-#     # save pitches
-#     def save_category(self):
-#         db.session.add(self)
-#         db.session.commit()
+    # save pitches
+    def save_category(self):
+        db.session.add(self)
+        db.session.commit()
 
-#     @classmethod
-#     def get_categories(cls):
-#         categories = PitchCategory.query.all()
-#         return categories
+    @classmethod
+    def get_categories(cls):
+        categories = PitchCategory.query.all()
+        return categories
 
 # #pitches class
 # class Pitch(db.Model):
